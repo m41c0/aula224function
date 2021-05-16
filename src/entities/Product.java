70ee2078d81329648020f67bd87bed483b0c5c5d
Maicon Ferreira <m41c0n@outlook.com>
2021-05-16 19:36:17 -0300

    Reference method com método estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -25,12 +25,14 @@ public class Product {
 		this.price = price;
 	}
 
+
+	public static String staticUpperCaseName(Product p) {
+		return p.getName().toUpperCase();
+	}
+	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price) ;
 	}
-
-
-
 
 }
